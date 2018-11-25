20a764cf2fe9a39d06addb6a8183b2ebd6bc23f0
Luka Jovanovic <lukajvnv@gmail.com>
2018-11-25 13:36:57 +0100

    Dodavanje kostura projekta.

---
+++ airFlights/src/main/java/com/airFlights/AirFlightsApplication.java
@@ -0,0 +1,12 @@
+package com.airFlights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirFlightsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirFlightsApplication.class, args);
+	}
+}
